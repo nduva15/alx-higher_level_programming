a2f9620b64a3cbe765d00ef01cf7523338c0fb12
nduva15 <timothynduva349@gmail.com>
2022-12-19 22:17:27 -0800

    javascript-warm-up

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.callMeMoby = function (x, callback) {
+    
+    for (let i = 0; i < x; i++) {
+	
+	callback();
+	
+    }
+    
+};
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+exports.add = function (a, b) {
+    
+    return (a + b);
+    
+};
